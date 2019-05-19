94660d1e0a433c950307d05a878b0df673d81ce0
pwachira900@gmail.com <pwachira900@gmail.com>
2019-05-19 11:53:28 +0300

    add Giant bomb Service class

--- app/src/main/java/com/moringaschool/gamerpro/Constants.java
+++ app/src/main/java/com/moringaschool/gamerpro/Constants.java
@@ -1,4 +1,13 @@
 package com.moringaschool.gamerpro;
 
 public class Constants {
+    public static final String GIANTBOMB_TOKEN = BuildConfig.GIANTBOMB_TOKEN;
+
+    public static final String GIANTBOMB_BASE_URL = "http://www.giantbomb.com/api/games/";
+
+
+    public static final String GIANTBOMB_PLATFORMS__QUERY_PARAMETER = "platforms";
+
+
+
 }
