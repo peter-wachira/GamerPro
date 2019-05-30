3376f2229cdca08bc1173cc6d3f137cb7c313027
pwachira900@gmail.com <pwachira900@gmail.com>
2019-05-30 13:25:57 +0300

    add ons start drag  interface

--- app/src/main/java/com/moringaschool/gamerpro/util/ItemTouchHelperAdapter.java
+++ app/src/main/java/com/moringaschool/gamerpro/util/ItemTouchHelperAdapter.java
@@ -1,4 +1,6 @@
 package com.moringaschool.gamerpro.util;
 
-public class ItemTouchHelperAdapter {
+public interface ItemTouchHelperAdapter {
+    boolean onItemMove(int fromPosition, int toPosition);
+    void onItemDismiss(int position);
 }
---
+++ app/src/main/java/com/moringaschool/gamerpro/util/OnStartDragListener.java
@@ -0,0 +1,4 @@
+package com.moringaschool.gamerpro.util;
+
+public class OnStartDragListener {
+}
